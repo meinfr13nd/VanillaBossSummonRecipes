8380340643f9479c5a33ad494cbfefa71e591b03
Sarah Samora <sarah@nicholsons.co>
2023-03-25 22:55:04 -0700

    Add queen slime summon recipes

--- VanillaBossSummonRecipes.cs
+++ VanillaBossSummonRecipes.cs
@@ -1,8 +1,25 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace VanillaBossSummonRecipes
 {
 	public class VanillaBossSummonRecipes : Mod
 	{
-	}
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = Recipe.Create(ItemID.QueenSlimeCrystal, 1);
+            recipe.AddIngredient(ItemID.PinkGel, 20);
+            recipe.AddIngredient(ItemID.PlatinumCrown, 1);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+
+            recipe = Recipe.Create(ItemID.QueenSlimeCrystal, 1);
+            recipe.AddIngredient(ItemID.PinkGel, 20);
+            recipe.AddIngredient(ItemID.GoldCrown, 1);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+        }
+    }
 }
